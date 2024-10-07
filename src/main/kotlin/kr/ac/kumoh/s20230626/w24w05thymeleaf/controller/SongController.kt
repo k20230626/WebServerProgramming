bdff8366de067640d6da8ee220a62d04072645a8
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2024-10-07 16:44:30 +0900

    10/7

--- src/main/kotlin/kr/ac/kumoh/s20230626/w24w05thymeleaf/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20230626/w24w05thymeleaf/controller/SongController.kt
@@ -1,12 +1,15 @@
 package kr.ac.kumoh.s20230626.w24w05thymeleaf.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class SongController {
+
     @GetMapping("/song/random")
-    fun getRandomSong(): String {
+    fun getRandomSong(model: Model): String {
+        model.addAttribute("songTitle","UNDEAD")
         return "random"
     }
 }
