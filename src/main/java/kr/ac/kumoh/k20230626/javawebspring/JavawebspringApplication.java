305d57b38d0616d557c271762f5af24ebb43b5a6
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2024-10-24 14:30:17 +0900

    init

---
+++ src/main/java/kr/ac/kumoh/k20230626/javawebspring/JavawebspringApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.k20230626.javawebspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawebspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavawebspringApplication.class, args);
+    }
+
+}
