857b3002b70be91124881c2781779c5f5c402bc7
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2024-11-06 15:14:48 +0900

    init

---
+++ src/main/kotlin/kr/ac/kumoh/s20230626/hw01/Hw01Application.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20230626.hw01
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Hw01Application
+
+fun main(args: Array<String>) {
+    runApplication<Hw01Application>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20230626/hw01/Hw01ApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20230626.hw01
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Hw01ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
