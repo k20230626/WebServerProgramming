94031d91f8274e50b31bd173ea7e9cf8c04a895a
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2024-12-05 13:26:34 +0900

    init

---
+++ src/test/kotlin/kr/ac/kumoh/s20230626/mongo/MongoApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20230626.mongo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
