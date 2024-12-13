09bb2e6e93708c8a28e12d60bfc63617b78da943
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2024-12-13 11:43:59 +0900

    init

---
+++ src/main/kotlin/kr/ac/kumoh/s20230626/termproject/TermProjectApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20230626.termproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TermProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<TermProjectApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20230626/termproject/TermProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20230626.termproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TermProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
